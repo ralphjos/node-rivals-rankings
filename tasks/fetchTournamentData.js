e1a760aecc468ab4fdeeefc46b1b6716e353122a
Ralph Arroyo <ralph_arroyo@gap.com>
2016-06-14 16:48:41 -0700

    refactors fetchTournamentData.js

--- tasks/fetchTournamentData.js
+++ tasks/fetchTournamentData.js
@@ -1,26 +1,19 @@
 /**
- * Bootstrap
- * (sails.config.bootstrap)
- *
- * An asynchronous bootstrap function that runs before your Sails app gets lifted.
- * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
- *
- * For more information on bootstrapping your app, check out:
- * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
+ * This module is called by config/bootstrap.js which is ran before sails lift completes
  */
 var log = require('captains-log')();
 var http = require('http');
 var https = require('https');
 var fetchMatches = require('./fetchMatches.js');
-var simpleRequest = require('./simpleRequest.js');
+var fetch = require('node-fetch');
 
-var whiteList = ["west", "wcs", "central", "ccs", "east", "ecs", "getgood", "national", "ncs"];
+const WHITE_LIST = ["west", "wcs", "central", "ccs", "east", "ecs", "getgood", "national", "ncs"];
 
 function whiteListed(tournamentUrl) {
       tournamentUrl = tournamentUrl.toLowerCase();
 
-      for(var i = 0; i < whiteList.length; i++) {
-            if (tournamentUrl.includes(whiteList[i])) {
+      for (var j = 0; j < WHITE_LIST.length; j++) {
+            if (tournamentUrl.includes(WHITE_LIST[j])) {
                   return true;
             }
       }
@@ -31,47 +24,45 @@ function whiteListed(tournamentUrl) {
 module.exports = function () {
 
       // API Fetch Variable Setup
-      var api_key = sails.config.challonge.key;
-      var created_after = '2016-01-10';
-      var options = {
-            hostname: 'api.challonge.com',
-            path: '/v1/tournaments.json?api_key=' + api_key + '&created_after=' + created_after,
-            method: 'GET'
-      };
+      const apiKey = sails.config.challonge.key;
+      const createdAfter = '2016-01-10';
+      const state = 'ended';
 
-      simpleRequest(options, function (allTournaments) {
-            if (!allTournaments) {
-                  log("Request to fetch all tournaments failed.")
-            }
+      function printFoo() {
+            log("foo");
+      }
 
-            allTournaments.forEach(function (listItem, index) {
-                  log(listItem.tournament.url, whiteListed(listItem.tournament.url));
-                  if (whiteListed(listItem.tournament.url)) {
-
-                        Tournament.findOne({
-                              tournamentID: listItem.tournament.id
-                        }).exec(function (err, record) {
-
-                              if (err) {
-                                    return;
-                              }
-
-
-                              // If not existing, add to database
-                              if (!record) {
-                                    Tournament.create({
-                                          tournamentID: listItem.tournament.id,
-                                          tournamentName: listItem.tournament.name,
-                                          date: listItem.tournament.started_at
-                                    }).exec(function createCB(err, created) {
-                                          log.info('"%s" Not Found In DB - Adding', listItem.tournament.name);
-                                    });
-
-                              }
-
-                              fetchMatches(listItem.tournament.id);
-                        });
+      fetch('https://api.challonge.com/v1/tournaments.json?api_key=' + apiKey + '&created_after=' + createdAfter + '&state=' + state)
+            .then(function (res) {
+                  return res.json();
+            })
+            .then(function (allTournaments) {
+                  var rankedTournaments = [];
+                  for (var i = 0; i < allTournaments.length; i++) {
+                        var tournament = allTournaments[i];
+                        if (whiteListed(tournament.tournament.url)) {
+                              rankedTournaments.push(tournament)
+                        }
                   }
+                  return rankedTournaments;
+            })
+            .then(function (rankedTournaments) {
+                  var findCriteria = rankedTournaments.map(function (rankedTournament) {
+                        return {
+                              tournamentId: rankedTournament.tournament.id
+                        }
+                  });
+
+                  var recordsToCreate = rankedTournaments.map(function (rankedTournament) {
+                        return {
+                              tournamentID: rankedTournament.tournament.id,
+                              tournamentName: rankedTournament.tournament.name,
+                              date: rankedTournament.tournament.started_at
+                        }
+                  });
+
+                  Tournament.findOrCreate(findCriteria, recordsToCreate).then(function () {
+                        log("all tournaments added!");
+                  });
             });
-      });
 };
