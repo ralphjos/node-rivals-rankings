328a20b6f7eb92e50b93a48d6228ceeb0a1bb944
Ralph Arroyo <ralphjos@berkeley.edu>
2016-06-19 22:35:47 -0700

    counts wins and losses of each player

--- tasks/runGlicko2.js
+++ tasks/runGlicko2.js
@@ -18,17 +18,25 @@ var ranking = new glicko2.Glicko2(settings);
 module.exports = function (tournamentInfo) {
       var allPlayers = tournamentInfo.playerNames;
       var playerRatings = {};
+      var playerWinsCount = {};
+      var playerLossesCount = {};
       var matches = [];
 
       return Promise.map(allPlayers, function (challongeUsername) {
             return Player.findOne({challongeUsername: challongeUsername}).then(function (player) {
                   playerRatings[challongeUsername] =
                         ranking.makePlayer(player.ratingMu, player.ratingSigma, player.ratingVol);
+                  playerWinsCount[challongeUsername] = player.matchWinsCount;
+                  playerLossesCount[challongeUsername] = player.matchLossesCount;
             });
       }).then(function () {
-            return Promise.map(tournamentInfo.matchResults, function (matchResult) {
-                  var winner = playerRatings[matchResult.winnerName];
-                  var loser = playerRatings[matchResult.loserName];
+            return Promise.each(tournamentInfo.matchResults, function (matchResult) {
+                  var winnerName = matchResult.winnerName;
+                  var loserName = matchResult.loserName;
+                  var winner = playerRatings[winnerName];
+                  var loser = playerRatings[loserName];
+                  playerWinsCount[winnerName]++;
+                  playerLossesCount[loserName]++;
                   matches.push([winner, loser, 1]);
             }).then(function () {
                   ranking.updateRatings(matches);
@@ -36,15 +44,17 @@ module.exports = function (tournamentInfo) {
       }).then(function () {
             return Promise.map(allPlayers, function (playerName) {
                   var playerRating = playerRatings[playerName];
-                  Player.update({challongeUsername: playerName},
+                  return Player.update({challongeUsername: playerName},
                         {
                               ratingMu: playerRating.getRating(),
                               ratingSigma: playerRating.getRd(),
                               ratingVol: playerRating.getVol(),
-                              conservativeRating: playerRating.getRating() - (1.75 * playerRating.getRd())
+                              conservativeRating: playerRating.getRating() - (1.75 * playerRating.getRd()),
+                              matchWinsCount: playerWinsCount[playerName],
+                              matchLossesCount: playerLossesCount[playerName]
                         }
                   ).then(function (player) {
-                        // updated
+
                   });
             });
       });
